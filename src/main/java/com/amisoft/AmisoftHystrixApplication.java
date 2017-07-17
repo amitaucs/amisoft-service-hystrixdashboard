8da14646efa8fdec3c340cebf7c0c5e5b43614e3
amitdatta <the.amistudy@gmail.com>
2017-07-17 18:04:17 +0530

    First commit

---
+++ src/main/java/com/amisoft/AmisoftHystrixApplication.java
@@ -0,0 +1,16 @@
+package com.amisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+public class AmisoftHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmisoftHystrixApplication.class, args);
+	}
+}
